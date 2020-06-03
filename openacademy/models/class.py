4caf214712b82f36aa5e4782ece92f14a66674dd
houriatalouzet <houria.talouzet@auguria.fr>
2020-06-03 14:44:05 +0200

    first devs of openacademy module

---
+++ openacademy/models/class.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ResClass(models.Model):
+    _inherit = "res.class"
+
+    name = fields.Float(string="Class name")
+    tutor_id = fields.Char(string="Tutor")
+    Coefficient = fields.Float(string="Coefficient")
+    
